13daa00efea095161e8ebdbd21de1bdb3231abd6
Nazmul Ali  Biswas <nazmulali8262@gmail.com>
2024-11-07 01:44:19 +0600

    form-validation-using-google-sheet by @nazmulalibiswas-v1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+//@nazmulalibiswas
+const scriptURL = 'https://script.google.com/macros/s/AKfycby3NvDR41S2IMBSh_YSoqqlrHVkORMLDh7HNjQ4hD0uRmMF-CbuLbAmsSdF_dkgrrRSuA/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
